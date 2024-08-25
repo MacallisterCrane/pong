341dc239d310172534699047ede5063b51f595f1
Macallister Crane <macallistercrane@yahoo.com>
2024-08-25 04:35:41 +0000

    Score class

---
+++ pong_score.py
@@ -0,0 +1,45 @@
+from turtle import Turtle
+
+
+class Score(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.goto(-100, 150)
+        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
+        self.goto(100, 150)
+        self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
+
+    def l_point(self):
+        self.l_score += 1
+        self.update_score()
+
+    def r_point(self):
+        self.r_score += 1
+        self.update_score()
+
+
+class SideSeparation(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.goto(0, -310)
+        self.setheading(90)
+        self.draw_line()
+
+    def draw_line(self):
+        self.pendown()
+        self.fd(10)
+        self.penup()
+        self.fd(10)
